49caed4d7509e9232dbbbf07387aa1f8a96c51ba
brighit0805 <a20190802@pucp.edu.pe>
2024-09-26 19:39:42 -0500

    first commit

---
+++ src/test/java/com/example/lab5_20190802/Lab520190802ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20190802;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520190802ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
